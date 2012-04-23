e92d9c8a6d9ab95d0ef28370ddc6a5c91e3b18c3
Cedric Legendre <cedric.legendre@invenietis.com>
2012-04-23 13:09:00 +0200

    change default user names into their english version.

--- DBPackages/CK.Actor/Sql/Model.CK.Actor.Install.1.0.0.sql
+++ DBPackages/CK.Actor/Sql/Model.CK.Actor.Install.1.0.0.sql
@@ -1,18 +1,21 @@
 
-create table CK.tActor (
+create table CK.tActor 
+(
   ActorId int not null identity ( 2, 1 ),
-  constraint PK_tActor primary key clustered ( ActorId )
+  constraint PK_tActor primary key clustered ( ActorId ) on [PRIMARY]
 );
 
 
-create table CK.tUser (
+create table CK.tUser 
+(
   UserId int not null,
   UserName nvarchar( 32 ) not null,
   Email varchar( 100 ) not null,
   CreationDate DateTime not null constraint DF_tUser_CreationDate default ( getutcdate() ),
   LastModificationDate DateTime not null constraint DF_tUser_LastModificationDate default ( getutcdate() ),
   IsEnabled bit not null constraint DF_tUser_IsEnabled default ( 1 ),
-  constraint PK_tUser primary key ( UserId ),
+
+  constraint PK_tUser primary key  clustered ( UserId ) on [PRIMARY],
   constraint UK_tUser_UserName unique ( UserName ),
   constraint FK_tUser_tActor foreign key ( UserId ) references CK.tActor ( ActorId )
 );
@@ -25,11 +28,11 @@ GO
 
 -- 0 == Anonymous user
 insert into CK.tActor ( ActorId ) values ( 0 );
-insert into CK.tUser ( UserId, UserName, Email ) values ( 0, N'Anonyme', '' );
+insert into CK.tUser ( UserId, UserName, Email ) values ( 0, N'Anonymous', '' );
 
 -- 1 == System user
 insert into CK.tActor ( ActorId ) values ( 1 );
-insert into CK.tUser ( UserId, UserName, Email ) values ( 1, N'Système', '' );
+insert into CK.tUser ( UserId, UserName, Email ) values ( 1, N'System', '' );
 
 GO
 
