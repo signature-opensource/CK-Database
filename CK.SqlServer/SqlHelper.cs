277399a3777a56f02daa1dc6e069fdc59945a836
Cedric <cedric.legendre@invenietis.fr>
2013-03-07 13:15:41 +0100

    Support Structured SqlDbType.

--- CK.SqlServer/SqlHelper.cs
+++ CK.SqlServer/SqlHelper.cs
@@ -92,6 +92,8 @@ namespace CK.SqlServer
                 case SqlDbType.Real: return Convert.ToString( v, CultureInfo.InvariantCulture );
                 case SqlDbType.Money: return Convert.ToString( v, CultureInfo.InvariantCulture );
                 case SqlDbType.Xml: return String.Format( "cast( '{0}' as xml )", SqlEncode( Convert.ToString( v, CultureInfo.InvariantCulture ) ) );
+                case SqlDbType.Structured: return Convert.ToString( v, CultureInfo.InvariantCulture );
+
                 default: throw new Exception( "No sql string representation for:" + dbType.ToString() );
             }
         }
