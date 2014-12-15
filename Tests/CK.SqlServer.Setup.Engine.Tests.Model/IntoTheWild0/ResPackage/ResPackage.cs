814aed234e9e97a0a271e444905e18c0c5e07a1c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-12-15 11:00:02 +0100

    Minor changes.

--- Tests/CK.SqlServer.Setup.Engine.Tests.Model/IntoTheWild0/ResPackage/ResPackage.cs
+++ Tests/CK.SqlServer.Setup.Engine.Tests.Model/IntoTheWild0/ResPackage/ResPackage.cs
@@ -4,7 +4,7 @@ using CK.SqlServer.Setup;
 
 namespace IntoTheWild0
 {
-    [SqlPackage( Schema = "CK", HasModel=true, Database = typeof(SqlDefaultDatabase), ResourceType = typeof(ResPackage), ResourcePath="ResPackage.Resource" ), Versions( "2.9.2" )]
+    [SqlPackage( Schema = "CK", HasModel = true, Database = typeof( SqlDefaultDatabase ), ResourceType = typeof( ResPackage ), ResourcePath = "ResPackage.Resource" ), Versions( "2.9.2" )]
     public class ResPackage : SqlPackage
     {
     }
