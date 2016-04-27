b61d0174aad32555cfe4bd89beb84fd78076830e
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2016-04-27 11:43:00 +0200

    Added base CkDbSetup console project

---
+++ CkDbSetup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace CkDbSetup
+{
+    class Program
+    {
+        static int Main( string[] args )
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "CkDbSetup",
+                Description = "Database setup utilities for CK.Database assemblies",
+                FullName = "CK.Database setup console utility"
+            };
+
+            app.OnExecute( () =>
+            {
+                app.ShowHelp();
+                return 2;
+            } );
+
+            return app.Execute( args );
+        }
+    }
+}
