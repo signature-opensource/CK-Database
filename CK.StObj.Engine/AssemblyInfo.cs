bcd262ed60a0b1cd3b54e7cf7010fc56bc50b6b0
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-10-22 18:45:50 +0200

    Bad constraint!! CKSetup.Runner is the runner!

--- CK.StObj.Engine/AssemblyInfo.cs
+++ CK.StObj.Engine/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 [assembly: CK.Setup.IsSetupDependency()]
 // Version 8.0.1--0026-develop introduces IsModelDependent and IsModelDependentSource.
 // The new CK.SqlServer uses it.
-[assembly: CK.Setup.RequiredSetupDependency("CK.Setup.Runner", minDependencyVersion: "8.0.1--0026-develop" )]
+[assembly: CK.Setup.RequiredSetupDependency("CKSetup.Runner", minDependencyVersion: "8.0.1--0026-develop" )]
