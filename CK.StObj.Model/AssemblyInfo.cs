3fa5d6790940a58c65f24551c26e60d879591c9e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-10-22 14:17:41 +0200

    Added minDependencyVersion from Model to Runtime to handle Ambient services.

--- CK.StObj.Model/AssemblyInfo.cs
+++ CK.StObj.Model/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 
 [assembly: CK.Setup.IsModel()]
-[assembly: CK.Setup.RequiredSetupDependency( "CK.StObj.Runtime" )]
+// Version 8.0.1--0038-develop introduces Ambient Services resolution.
+[assembly: CK.Setup.RequiredSetupDependency( "CK.StObj.Runtime", minDependencyVersion: "8.0.1--0038-develop" )]
