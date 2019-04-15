1eb42472ec9ed2ec52743e3788c81cf82a2803ae
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-04-15 17:49:16 +0200

    Fix unhandled fire and forget task in test.

--- Tests/SqlCallDemo/SqlCallDemo/ProviderDemo/ProviderDemoPackage.cs
+++ Tests/SqlCallDemo/SqlCallDemo/ProviderDemo/ProviderDemoPackage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -34,19 +34,19 @@ namespace SqlCallDemo.ProviderDemo
         public abstract Task<string> TenantOnlyAsync( [ParameterSource]ITenantCallContext ctx );
 
         [SqlProcedure( "sActorCulture" )]
-        public abstract Task<string> ActorCulture( [ParameterSource]IActorCultureCallContext ctx );
+        public abstract string ActorCulture( [ParameterSource]IActorCultureCallContext ctx );
 
         [SqlProcedure( "sActorCulture" )]
         public abstract Task<string> ActorCultureAsync( [ParameterSource]IActorCultureCallContext ctx );
 
         [SqlProcedure( "sCultureTenant" )]
-        public abstract Task<string> CultureTenant( [ParameterSource]ICultureTenantCallContext ctx );
+        public abstract string CultureTenant( [ParameterSource]ICultureTenantCallContext ctx );
 
         [SqlProcedure( "sCultureTenant" )]
         public abstract Task<string> CultureTenantAsync( [ParameterSource]ICultureTenantCallContext ctx );
 
         [SqlProcedure( "sAllContexts" )]
-        public abstract Task<string> AllContexts( [ParameterSource]IAllCallContext ctx );
+        public abstract string AllContexts( [ParameterSource]IAllCallContext ctx );
 
         [SqlProcedure( "sAllContexts" )]
         public abstract Task<string> AllContextsAsync( [ParameterSource]IAllCallContext ctx );
