4e16cf2f0bf6570ebe4c3072f21f9142f7cb4529
Olivier Spinelli <olivier.spinelli@signature-code.com>
2019-08-16 13:00:46 +0200

    Removed SimpleServiceContainer AddStObjMap.
    If needed, it will have to be rewritten.

--- Tests/SqlCallDemo/SqlCallDemo/TransactionalPackage.cs
+++ Tests/SqlCallDemo/SqlCallDemo/TransactionalPackage.cs
@@ -6,7 +6,8 @@ namespace SqlCallDemo
     [SqlPackage( Schema = "CK", ResourcePath = "Res" ), Versions( "19.01.14" )]
     public abstract class TransactionalPackage : SqlPackage
     {
-        // This sets the SERIALIZABLE level but the change is scoped to the stored procedure.
+        // The sTransactSetLevelNotWorking procedure sets the SERIALIZABLE level but the
+        // change is scoped to the stored procedure!
         [SqlProcedure( "sTransactSetLevelNotWorking" )]
         public abstract string TransactSetLevelNotWorking( ISqlTransactionCallContext c );
     }
