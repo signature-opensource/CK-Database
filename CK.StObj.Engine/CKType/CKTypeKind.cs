f4e327b7d57d16e96f836d5abb3dd10b2ffe0dba
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-01-16 17:52:13 +0100

    Implemented Front type detection (no test yet).

--- CK.StObj.Engine/CKType/CKTypeKind.cs
+++ CK.StObj.Engine/CKType/CKTypeKind.cs
@@ -17,19 +17,19 @@ namespace CK.Setup
         None,
 
         /// <summary>
-        /// Auto service flag. 
+        /// Auto service flag. This flag is set if and only if the type is marked with a <see cref="IAutoService"/> interface marker.
         /// </summary>
         IsAutoService = 1,
 
         /// <summary>
         /// Singleton flag.
-        /// External services are flagged with this only.
+        /// External services are flagged with this only (or with this and <see cref="IsMarshallableService"/> or <see cref="IsFrontOnlyService"/>).
         /// </summary>
         IsSingleton = 2,
 
         /// <summary>
         /// Scoped flag.
-        /// External services are flagged with this only.
+        /// External services are flagged with this only (or with this and <see cref="IsMarshallableService"/> or <see cref="IsFrontOnlyService"/>).
         /// </summary>
         IsScoped = 4,
 
@@ -37,6 +37,26 @@ namespace CK.Setup
         /// A singleton auto service: <see cref="IsAutoService"/> | <see cref="IsSingleton"/>. 
         /// </summary>
         AutoSingleton = IsAutoService | IsSingleton,
+
+        /// <summary>
+        /// A singleton front only auto service: <see cref="IsAutoService"/> | <see cref="IsSingleton"/> | <see cref="IsFrontOnlyService"/>. 
+        /// </summary>
+        AutoFrontOnlySingleton = AutoSingleton | IsFrontOnlyService,
+
+        /// <summary>
+        /// A singleton marshallable auto service: <see cref="IsAutoService"/> | <see cref="IsSingleton"/> | <see cref="IsMarshallableService"/>. 
+        /// </summary>
+        AutoMarshallableSingleton = AutoSingleton | IsMarshallableService,
+
+        /// <summary>
+        /// A singleton, front only, externally declared service: <see cref="IsSingleton"/> | <see cref="IsFrontOnlyService"/>. 
+        /// </summary>
+        FrontOnlySingleton = IsSingleton | IsFrontOnlyService,
+
+        /// <summary>
+        /// A singleton, marshallable, externally declared service: <see cref="IsSingleton"/> | <see cref="IsMarshallableService"/>. 
+        /// </summary>
+        MarshallableSingleton = IsSingleton | IsMarshallableService,
 
         /// <summary>
         /// A real object is a singleton. 
@@ -49,14 +69,39 @@ namespace CK.Setup
         AutoScoped = IsAutoService | IsScoped,
 
         /// <summary>
+        /// A singleton front only auto service: <see cref="IsAutoService"/> | <see cref="IsScoped"/> | <see cref="IsFrontOnlyService"/>. 
+        /// </summary>
+        AutoFrontOnlyScoped = AutoScoped | IsFrontOnlyService,
+
+        /// <summary>
+        /// A singleton marshallable auto service: <see cref="IsAutoService"/> | <see cref="IsScoped"/> | <see cref="IsMarshallableService"/>. 
+        /// </summary>
+        AutoMarshallableScoped = AutoScoped | IsMarshallableService,
+
+        /// <summary>
+        /// A singleton, front only, externally declared service: <see cref="IsScoped"/> | <see cref="IsFrontOnlyService"/>. 
+        /// </summary>
+        FrontOnlyScoped = IsScoped | IsFrontOnlyService,
+
+        /// <summary>
+        /// A singleton, marshallable, externally declared service: <see cref="IsScoped"/> | <see cref="IsMarshallableService"/>. 
+        /// </summary>
+        MarshallableScoped = IsScoped | IsMarshallableService,
+
+        /// <summary>
         /// A IPoco marked interface.
         /// </summary>
         IsPoco = 16,
 
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //IsFront = 32
+        /// <summary>
+        /// Front only service. This excludes <see cref="IsMarshallableService"/>.
+        /// </summary>
+        IsFrontOnlyService = 32,
+
+        /// <summary>
+        /// Marshallable service. This excludes <see cref="IsFrontOnlyService"/>.
+        /// </summary>
+        IsMarshallableService = 64
     }
 
     /// <summary>
@@ -78,10 +123,18 @@ namespace CK.Setup
                 case CKTypeKind.None: return "None";
                 case CKTypeKind.RealObject: return "RealObject";
                 case CKTypeKind.AutoSingleton: return "SingletonAutoService";
+                case CKTypeKind.AutoFrontOnlySingleton: return "SingletonFrontOnlyAutoService";
+                case CKTypeKind.AutoMarshallableSingleton: return "SingletonMarshallableAutoService";
                 case CKTypeKind.AutoScoped: return "ScopedAutoService";
-                case CKTypeKind.IsScoped: return "ScopedService";
-                case CKTypeKind.IsSingleton: return "SingletonService";
+                case CKTypeKind.AutoFrontOnlyScoped: return "ScopedFrontOnlyAutoService";
+                case CKTypeKind.AutoMarshallableScoped: return "ScopedMarshallableAutoService";
                 case CKTypeKind.IsAutoService: return "AutoService";
+                case CKTypeKind.IsScoped: return "ExternallyDefinedScopedService";
+                case CKTypeKind.MarshallableScoped: return "ExternallyDefinedMarshallableScopedService";
+                case CKTypeKind.FrontOnlyScoped: return "ExternallyDefinedFrontOnlyScopedService";
+                case CKTypeKind.IsSingleton: return "ExternallyDefinedSingletonService";
+                case CKTypeKind.MarshallableSingleton: return "ExternallyDefinedMarshallableSingletonService";
+                case CKTypeKind.FrontOnlySingleton: return "ExternallyDefinedFrontOnlySingletonService";
                 case CKTypeKind.IsPoco: return "Poco";
                 default:
                     {
@@ -118,6 +171,10 @@ namespace CK.Setup
             bool isAutoSingleton = (@this & CKTypeKind.AutoSingleton) == CKTypeKind.AutoSingleton;
             bool isRealObject = (@this & CKTypeKind.RealObject) == CKTypeKind.RealObject;
             bool isPoco = (@this & CKTypeKind.IsPoco) == CKTypeKind.IsPoco;
+
+            bool isMarshallable = (@this & CKTypeKind.IsMarshallableService) != 0;
+            bool isFrontOnly = (@this & CKTypeKind.IsFrontOnlyService) != 0;
+
             string conflict = null;
             if( isAutoScoped && isAutoSingleton )
             {
@@ -139,6 +196,12 @@ namespace CK.Setup
             else if( isPoco && isAuto )
             {
                 conflict = "Poco and AutoService";
+            }
+            // This should not happen unless the enum value is externally manipulated.
+            if( isMarshallable && isFrontOnly )
+            {
+                if( conflict != null ) conflict += " and ";
+                conflict += "both Marshallable and Front service";
             }
             return conflict == null ? null : $"Invalid CK type combination: {conflict} cannot be defined simultaneously."; 
         }
--- CK.StObj.Model/AutomaticDI/IMarshaller.cs
+++ CK.StObj.Model/AutomaticDI/IMarshaller.cs
@@ -10,12 +10,14 @@ namespace CK.StObj.Model
     /// thanks to any serialization/deserialization mechanism.
     /// Such marshaller implementations must be registered and available in the DI container as soon
     /// as the marshallable service is used from a remote/background context.
+    /// Whenever a marshaller interface is an Auto service, its registartion automatically declares the <typeparamref name="T"/>
+    /// to be a Marshallable service (even if T is not marked with any interface).
     /// </summary>
     /// <typeparam name="T">Type of the service to marshall.</typeparam>
-    public interface IMarshaller<T> where T : IMarshallableAutoService
+    public interface IMarshaller<T>
     {
         /// <summary>
-        /// Writes any information to the binary writer that <see cref="Read(ICKBinaryReader)"/> wil use to
+        /// Writes any information to the binary writer that <see cref="Read(ICKBinaryReader)"/> will use to
         /// instanciate a copy of the <paramref name="service"/>.
         /// </summary>
         /// <param name="writer">The writer.</param>
