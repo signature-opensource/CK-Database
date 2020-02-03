ebeb5dd59721484587bab051a377f449242773ed
Kuinox <nicolas.vandeginste@signature-code.com>
2020-02-03 10:23:25 +0100

    Version bump + Applied CKli settings+upgrade to netcore3.1 and dropped everything else.

--- CodeCakeBuilder/Build.StandardPushCKSetupComponents.cs
+++ CodeCakeBuilder/Build.StandardPushCKSetupComponents.cs
@@ -17,30 +17,15 @@ namespace CodeCake
         {
             return new CKSetupComponent[]{
 
-new CKSetupComponent( "CK.StObj.Model", "net461" ),
 new CKSetupComponent( "CK.StObj.Model", "netstandard2.0" ),
-new CKSetupComponent( "CK.StObj.Runtime", "net461" ),
-new CKSetupComponent( "CK.StObj.Runtime", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.StObj.Runtime", "netcoreapp2.2" ),
-new CKSetupComponent( "CK.StObj.Engine", "net461" ),
-new CKSetupComponent( "CK.StObj.Engine", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.StObj.Engine", "netcoreapp2.2" ),
-new CKSetupComponent( "CK.Setupable.Model", "net461" ),
+new CKSetupComponent( "CK.StObj.Runtime", "netcoreapp3.1" ),
+new CKSetupComponent( "CK.StObj.Engine", "netcoreapp3.1" ),
 new CKSetupComponent( "CK.Setupable.Model", "netstandard2.0" ),
-new CKSetupComponent( "CK.Setupable.Runtime", "net461" ),
-new CKSetupComponent( "CK.Setupable.Runtime", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.Setupable.Runtime", "netcoreapp2.2" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Model", "net461" ),
+new CKSetupComponent( "CK.Setupable.Runtime", "netcoreapp3.1" ),
 new CKSetupComponent( "CK.SqlServer.Setup.Model", "netstandard2.0" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Runtime", "net461" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Runtime", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Runtime", "netcoreapp2.2" ),
-new CKSetupComponent( "CK.Setupable.Engine", "net461" ),
-new CKSetupComponent( "CK.Setupable.Engine", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.Setupable.Engine", "netcoreapp2.2" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Engine", "net461" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Engine", "netcoreapp2.1" ),
-new CKSetupComponent( "CK.SqlServer.Setup.Engine", "netcoreapp2.2" )
+new CKSetupComponent( "CK.SqlServer.Setup.Runtime", "netcoreapp3.1" ),
+new CKSetupComponent( "CK.Setupable.Engine", "netcoreapp3.1" ),
+new CKSetupComponent( "CK.SqlServer.Setup.Engine", "netcoreapp3.1" )
 };
         }
 
--- Tests/LocalTestHelper/CKDatabaseLocalTestHelper.cs
+++ Tests/LocalTestHelper/CKDatabaseLocalTestHelper.cs
@@ -21,47 +21,29 @@ namespace CK.Testing
         IEnumerable<NormalizedPath> GetCKDatabaseComponentsPaths()
         {
             yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Model/bin/{_dbSetup.BuildConfiguration}/netstandard2.0" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Model/bin/{_dbSetup.BuildConfiguration}/net461" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Runtime/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Engine/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.StObj.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
 
             yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Model/bin/{_dbSetup.BuildConfiguration}/netstandard2.0" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Model/bin/{_dbSetup.BuildConfiguration}/net461" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Runtime/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Engine/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.Setupable.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
 
             yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Model/bin/{_dbSetup.BuildConfiguration}/netstandard2.0" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Model/bin/{_dbSetup.BuildConfiguration}/net461" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Runtime/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
 
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Engine/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"CK.SqlServer.Setup.Engine/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
         }
 
         IEnumerable<NormalizedPath> ICKDatabaseLocalTestHelperCore.SqlActorPackageComponentsPaths => GetSqlActorPackageComponentsPaths();
         IEnumerable<NormalizedPath> GetSqlActorPackageComponentsPaths()
         {
             yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage/bin/{_dbSetup.BuildConfiguration}/netstandard2.0" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage/bin/{_dbSetup.BuildConfiguration}/net461" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage.Runtime/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlActorPackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
         }
 
         IEnumerable<NormalizedPath> ICKDatabaseLocalTestHelperCore.SqlZonePackageComponentsPaths => GetSqlZonePackageComponentsPaths();
@@ -69,10 +51,7 @@ namespace CK.Testing
         {
             foreach( var p in GetSqlActorPackageComponentsPaths() ) yield return p;
             yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage/bin/{_dbSetup.BuildConfiguration}/netstandard2.0" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage/bin/{_dbSetup.BuildConfiguration}/net461" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.2" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp2.1" );
-            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage.Runtime/bin/{_dbSetup.BuildConfiguration}/net461" );
+            yield return _dbSetup.SolutionFolder.Combine( $"Tests/BasicModels/SqlZonePackage.Runtime/bin/{_dbSetup.BuildConfiguration}/netcoreapp3.1" );
         }
 
         IEnumerable<NormalizedPath> ICKDatabaseLocalTestHelperCore.AllLocalComponentsPaths => GetAllLocalComponentsPaths();
