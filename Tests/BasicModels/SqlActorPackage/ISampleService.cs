16da69dd4458b6210e39993512fa9df11fe584af
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-04-27 16:59:46 +0200

    Test for Scoped services in root TestHelper.AutomaticServices.

---
+++ Tests/BasicModels/SqlActorPackage/ISampleService.cs
@@ -0,0 +1,10 @@
+using CK.Core;
+using CK.SqlServer;
+
+namespace SqlActorPackage
+{
+    public interface ISampleService : IAutoService
+    {
+        int CreateGroup( string groupName );
+    }
+}
