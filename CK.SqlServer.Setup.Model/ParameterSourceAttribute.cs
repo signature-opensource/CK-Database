fe14d5edd75db835d16feeb681723c5b19bc8fd2
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-08-01 16:48:21 +0200

    Added internal IAllowUnclosedCommandAttribute.

--- CK.SqlServer.Setup.Model/ParameterSourceAttribute.cs
+++ CK.SqlServer.Setup.Model/ParameterSourceAttribute.cs
@@ -2,12 +2,15 @@ using System;
 
 namespace CK.Core;
 
+// Cris marker to allow ParameterSourceAttribute to not necessarily be
+// the IPoco closure of the IPoco command family.
+internal interface IAllowUnclosedCommandAttribute { }
 
 /// <summary>
 /// Decorates parameters of call methods to indicate that the properties of the parameter object 
 /// must be used to obtain actual parameter values.
 /// </summary>
 [AttributeUsage( AttributeTargets.Parameter )]
-public class ParameterSourceAttribute : Attribute
+public class ParameterSourceAttribute : Attribute, IAllowUnclosedCommandAttribute
 {
 }
